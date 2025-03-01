7e7a30a245038a5847fd78a4713d7152aae59329
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-01 16:06:10 -0300

    Add files via upload

---
+++ src/Testerepositorios.java
@@ -0,0 +1,41 @@
+import classes.*;
+import repositorios.*;
+
+
+public class Testerepositorios {
+    public static void main(String[] args) {
+
+        RepositorioProduto repositorioProduto = new RepositorioProduto();
+        Produto produto = repositorioProduto.getById(1);
+        System.out.println("Produto encontrado: " + (produto != null ? produto.getNome() : "Nenhum produto encontrado"));
+
+        RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
+        Usuario usuario = repositorioUsuario.getById(1);
+        System.out.println("Usuario encontrado: " + (usuario != null ? usuario.getNome_completo() : "Nenhum usuario encontrado"));
+
+        RepositorioVendas repositorioVendas =new RepositorioVendas();
+        Vendas vendas = repositorioVendas.getById(4);
+        System.out.println("Chave estrangeira usuario : " + (vendas != null ? vendas.getFk_usuario() : "Nenhuma chave estrangeira encontrada"));
+
+        RepositorioItensVenda repositorioItensVenda = new RepositorioItensVenda();
+        ItensVenda itensvenda = repositorioItensVenda.getById(3);
+        System.out.println("Repositorio itens venda: " + (itensvenda != null ? itensvenda.getFk_venda() : "Nenhum chave estrangeira de venda encontrada"));
+
+        RepositorioFormasPagamento repositorioFormasPagamento = new RepositorioFormasPagamento();
+        FormasPagamento formasPagamento =  repositorioFormasPagamento.getById(3);
+        System.out.println("Forma de pagamento encontrada: " +
+                (formasPagamento != null ? formasPagamento.getQuantParcelas() : "Nenhuma forma de pagamento encontrada"));
+
+        RepositorioEstoque repositorioEstoque = new RepositorioEstoque();
+        Estoque estoque = repositorioEstoque.getById(1);
+        System.out.println("Quantidade no estoque: " +
+                (estoque != null ? estoque.getQuantidade() : "Nenhuma quantidade de produto no estoque"));
+
+        RepositorioAvaliacao repositorioAvalicao = new RepositorioAvaliacao();
+        Avaliacao avaliaCao = repositorioAvalicao.getById(1);
+        System.out.println("Nota do produto ID 1: " +
+                (avaliaCao != null ? avaliaCao.getRating() : "Nenhuma nota encontrada"));
+
+
+    }
+}
