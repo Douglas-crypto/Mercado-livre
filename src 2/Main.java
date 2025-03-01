11256e4153a3d55e7bec09d3eedf1f1b889ce589
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-01 13:32:25 -0300

    Update Main.java
    Todos repositórios foram testados, com exceção de frete

--- src 2/Main.java
+++ src 2/Main.java
@@ -1,11 +1,5 @@
-import classes.AvaliaCao;
-import classes.Produto;
-import classes.Usuario;
-import classes.Vendas;
-import repositorios.RepositorioAvalicao;
-import repositorios.RepositorioProduto;
-import repositorios.RepositorioUsuario;
-import repositorios.RepositorioVendas;
+import classes.*;
+import repositorios.*;
 
 
 public class Main {
@@ -13,21 +7,33 @@ public class Main {
 
         RepositorioProduto repoProduto = new RepositorioProduto();
         Produto produto = repoProduto.getById(1);
-        System.out.println("classes.Produto encontrado: " + (produto != null ? produto.getNome() : "Nenhum produto encontrado"));
+        System.out.println("Produto encontrado: " + (produto != null ? produto.getNome() : "Nenhum produto encontrado"));
 
+        RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
+        Usuario usuario = repositorioUsuario.getById(1);
+        System.out.println("Usuario encontrado: " + (usuario != null ? usuario.getNome_completo() : "Nenhum usuario encontrado"));
 
-        RepositorioUsuario repoUsuario = new RepositorioUsuario();
-        Usuario usuario = repoUsuario.getById(1);
-        System.out.println("Usuário encontrado: " + (usuario != null ? usuario.getNome_completo() : "Nenhum usuário encontrado"));
+        RepositorioVendas repositorioVendas =new RepositorioVendas();
+        Vendas vendas = repositorioVendas.getById(4);
+        System.out.println("Chave estrangeira usuario : " + (vendas != null ? vendas.getFk_usuario() : "Nenhuma chave estrangeira encontrada"));
 
+        RepositorioItensVenda repositorioItensVenda = new RepositorioItensVenda();
+        ItensVenda itensVenda = repositorioItensVenda.getById(3);
+        System.out.println("Repositorio itens venda: " + (itensVenda != null ? itensVenda.getFk_venda() : "Nenhum chave estrangeira de venda encontrada"));
 
-        RepositorioVendas repoVendas = new RepositorioVendas();
-        Vendas venda = repoVendas.getById(1);
-        System.out.println("Venda encontrada: " + (venda != null ? "Venda ID " + venda.getId() : "Nenhuma venda encontrada"));
+        RepositorioFormasPagamento repositorioFormasPagamento = new RepositorioFormasPagamento();
+        FormasPagamento formasPagamento = repositorioFormasPagamento.getById(5);
+        System.out.println("Forma de pagamento encontrada: " +
+                (formasPagamento != null ? formasPagamento.getQuantParcelas() : "Nenhuma forma de pagamento encontrada"));
 
+        RepositorioEstoque repositorioEstoque = new RepositorioEstoque();
+        Estoque estoque = repositorioEstoque.getById(1);
+        System.out.println("Quantidade no estoque: " +
+                (estoque != null ? estoque.getQuantidade() : "Nenhuma quantidade de produto no estoque"));
 
-        RepositorioAvalicao repoAvaliacao = new RepositorioAvalicao();
-        AvaliaCao avaliacao = repoAvaliacao.getById(1);
-        System.out.println("Avaliação encontrada: " + (avaliacao != null ? "Rating " + avaliacao.getRating() : "Nenhuma avaliação encontrada"));
+        RepositorioAvalicao repositorioAvalicao = new RepositorioAvalicao();
+        AvaliaCao avaliaCao = repositorioAvalicao.getById(1);
+        System.out.println("Nota do produto ID 1: " +
+                (avaliaCao != null ? avaliaCao.getRating() : "Nenhuma nota encontrada"));
     }
 }
