fb10e182e544f0e5c185d706d6f6e599b8b73b44
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2025-03-03 17:47:27 -0300

    Add files via upload

---
+++ src/main/java/org/douglascosta/classes/Cadastros.java
@@ -0,0 +1,33 @@
+package org.douglascosta.classes;
+
+import org.douglascosta.database.ConnectionDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Cadastros {
+
+    public void adicionarUsuario(String username, String cpf_cnpj, String endereco, String email, String senha) {
+        String sql = "INSERT INTO cad_usuarios (username, cpf_cnpj, endereco, email, senha) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection connection = new ConnectionDB().getConnection();
+             PreparedStatement pstm = connection.prepareStatement(sql)) {
+
+            pstm.setString(1, username);
+            pstm.setString(2, cpf_cnpj);
+            pstm.setString(3, endereco);
+            pstm.setString(4, email);
+            pstm.setString(5, senha);
+
+            int linhaAfet = pstm.executeUpdate();
+
+            if (linhaAfet > 1) {
+                System.out.println("classes.Usuario cadastrado com exito");
+            } else {
+                System.out.println("classes.Usuario não cadastrado");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
