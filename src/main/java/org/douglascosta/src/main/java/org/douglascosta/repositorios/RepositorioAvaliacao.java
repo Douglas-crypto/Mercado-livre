bd71a57152d682c9263c2440e03e863c4fa87123
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-06 16:19:42 -0300

    Add files via upload

---
+++ src/main/java/org/douglascosta/src/main/java/org/douglascosta/repositorios/RepositorioAvaliacao.java
@@ -0,0 +1,38 @@
+package org.douglascosta.repositorios;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.douglascosta.classes.Avaliacao;
+import org.douglascosta.database.ConnectionDB;
+
+public class RepositorioAvaliacao {
+
+    public Avaliacao getById(long id) {
+        String sql = "SELECT * FROM avaliacao WHERE id = ?";
+
+        try (Connection conn = new ConnectionDB().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setLong(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    int fk_produto = rs.getInt("fk_produto");
+                    int fk_usuario = rs.getInt("fk_usuario");
+                    int rating = rs.getInt("nota_produto");
+                    String texto = rs.getString("texto");
+
+
+
+                    return new Avaliacao((int) id, fk_produto, fk_usuario, rating, texto);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao buscar usuário com ID: " + id);
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
